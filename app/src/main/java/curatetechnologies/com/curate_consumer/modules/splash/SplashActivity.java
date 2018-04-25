6ca515295c7d0e92b035faa30833597c1c7800df
mremondi <mremondi@colby.edu>
2018-04-25 14:08:10 -0400

    production release + fabric + app name is now determined by build flavor

--- app/src/main/java/curatetechnologies/com/curate_consumer/modules/splash/SplashActivity.java
+++ app/src/main/java/curatetechnologies/com/curate_consumer/modules/splash/SplashActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import curatetechnologies.com.curate_consumer.modules.login_or_signup.LoginOrSignUpActivity;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by mremondi on 2/12/18.
@@ -15,6 +17,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
 
         Intent intent = new Intent(this, LoginOrSignUpActivity.class);
         startActivity(intent);
