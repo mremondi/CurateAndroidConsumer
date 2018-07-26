9c5885ce7ac7bd811233e41985f5a5dfd6976fef
mremondi <mremondi@colby.edu>
2018-07-26 17:21:01 -0400

    reworked restaurant search UI

--- app/src/main/java/curatetechnologies/com/curate_consumer/presentation/ui/adapters/RestaurantSearchAdapter.java
+++ app/src/main/java/curatetechnologies/com/curate_consumer/presentation/ui/adapters/RestaurantSearchAdapter.java
@@ -1,5 +1,6 @@
 package curatetechnologies.com.curate_consumer.presentation.ui.adapters;
 
+import android.graphics.Color;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -11,12 +12,18 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.MultiTransformation;
 import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import curatetechnologies.com.curate_consumer.R;
+import curatetechnologies.com.curate_consumer.domain.model.ItemModel;
 import curatetechnologies.com.curate_consumer.domain.model.RestaurantModel;
 import curatetechnologies.com.curate_consumer.presentation.ui.views.listeners.RecyclerViewClickListener;
 import curatetechnologies.com.curate_consumer.presentation.ui.views.subclasses.RoundedCornerTransformation;
@@ -70,10 +77,12 @@ public class RestaurantSearchAdapter extends RecyclerView.Adapter<RestaurantSear
         ImageView restaurantLogo;
         @BindView(R.id.restaurant_search_view_holder_restaurant_name)
         TextView restaurantName;
-        @BindView(R.id.restaurant_search_view_holder_cuisine_type)
-        TextView restaurantCuisineType;
-        @BindView(R.id.restaurant_search_view_holder_restaurant_rating)
-        TextView restaurantRating;
+        @BindView(R.id.restaurant_search_view_holder_restaurant_ordering_enabled)
+        TextView orderingEnabled;
+        @BindView(R.id.restaurant_search_view_holder_distance)
+        TextView distance;
+        @BindView(R.id.restaurant_search_view_holder_restaurant_rating_pie)
+        PieChart ratingPie;
 
         public ViewHolder(CardView searchRow, RecyclerViewClickListener listener){
             super(searchRow);
@@ -87,18 +96,28 @@ public class RestaurantSearchAdapter extends RecyclerView.Adapter<RestaurantSear
             if (restaurant.getLogoURL() != null){
                 Glide.with(view)
                         .load(restaurant.getLogoURL())
-                        .apply(bitmapTransform(new MultiTransformation(
-                                new CenterCrop(), new RoundedCornerTransformation(45, 0,
-                                RoundedCornerTransformation.CornerType.ALL))))
                         .into(restaurantLogo);
             } else {
 
             }
             restaurantName.setText(restaurant.getName());
-            // TODO: restaurantRating.setText(restaurant.getRating());
-            // TODO: restaurantCuisineType.setText(restaurant.getCuisines());
-            restaurantCuisineType.setVisibility(View.GONE);
-            restaurantRating.setVisibility(View.GONE);
+
+            if (restaurant.getStripeID() != null && !restaurant.getStripeID().equals("undefined") &&
+                    !restaurant.getStripeID().equals("")) {
+                orderingEnabled.setTextColor(view.getResources().getColor(R.color.selectedGreen));
+                orderingEnabled.setText("Ordering Enabled");
+            } else {
+                orderingEnabled.setTextColor(view.getResources().getColor(R.color.colorAccentLight));
+                orderingEnabled.setText("Ordering Not Enabled");
+            }
+            distance.setText(restaurant.getDistance_in_mi());
+
+            if (restaurant.getRating()>0){
+                ratingPie.setVisibility(View.VISIBLE);
+                configurePieChart(restaurant);
+            } else{
+                ratingPie.setVisibility(View.INVISIBLE);
+            }
         }
 
         // -- BEGIN View.OnClickListener methods
@@ -107,5 +126,40 @@ public class RestaurantSearchAdapter extends RecyclerView.Adapter<RestaurantSear
             mListener.onClick(view, getAdapterPosition());
         }
         // -- END View.OnClickListener methods
+
+        private void configurePieChart(RestaurantModel restaurantModel) {
+            List<PieEntry> entries = calculateRatingEntries(restaurantModel.getRating());
+            PieDataSet set = new PieDataSet(entries, "");
+            set.setDrawIcons(false);
+            set.setDrawValues(false);
+            List<Integer> colors = new ArrayList<Integer>();
+            colors.add(Color.LTGRAY);
+            colors.add(view.getResources().getColor(R.color.selectedGreen));
+            set.setColors(colors);
+            set.setSelectionShift(0);
+            PieData data = new PieData(set);
+
+            ratingPie.setRotationEnabled(false);
+            ratingPie.setHighlightPerTapEnabled(false);
+            ratingPie.getLegend().setEnabled(false);
+            ratingPie.setUsePercentValues(false);
+            ratingPie.setDrawSlicesUnderHole(false);
+            ratingPie.setDrawHoleEnabled(true);
+            ratingPie.setHoleRadius(80f);
+            ratingPie.getDescription().setEnabled(false);
+            ratingPie.setDrawCenterText(true);
+            double percentRating = restaurantModel.getRating() * 100;
+            ratingPie.setCenterText(String.format("%.0f", percentRating));
+            ratingPie.setData(data);
+            ratingPie.invalidate(); // refresh
+        }
+
+        private List<PieEntry> calculateRatingEntries(Double rating) {
+            float ratingf = rating.floatValue();
+            List<PieEntry> entries = new ArrayList<PieEntry>();
+            entries.add(new PieEntry(1.0f - ratingf));
+            entries.add(new PieEntry(ratingf));
+            return entries;
+        }
     }
 }
