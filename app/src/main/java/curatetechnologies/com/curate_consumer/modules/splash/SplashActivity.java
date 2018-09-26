15993fcd0edb6e1f9758021d0ecb3e9e73118cca
mremondi <mremondi@colby.edu>
2018-09-26 14:59:14 -0400

    updated gradle and removed fabric/crashlytics

--- app/src/main/java/curatetechnologies/com/curate_consumer/modules/splash/SplashActivity.java
+++ app/src/main/java/curatetechnologies/com/curate_consumer/modules/splash/SplashActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import curatetechnologies.com.curate_consumer.modules.login_or_signup.LoginOrSignUpActivity;
-import com.crashlytics.android.Crashlytics;
-import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by mremondi on 2/12/18.
@@ -17,7 +15,6 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
 
         Intent intent = new Intent(this, LoginOrSignUpActivity.class);
         startActivity(intent);
